3df356178fb0bac87f002053304e91a07aa90aeb
Nitesh kumar Vishvakarma <147709126+NICK-k29@users.noreply.github.com>
2025-03-26 15:02:26 +0530

    Add files via upload

---
+++ python-Portfolio/config.py
@@ -0,0 +1,11 @@
+import os
+
+SECRET_KEY = "your_secret_key"
+SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+MAIL_SERVER = "smtp.gmail.com"
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = "niteshvishvakarma29@gmail.com"
+MAIL_PASSWORD = "12345@Nk"
